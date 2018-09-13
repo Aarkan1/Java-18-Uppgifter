3e5c5c58cfa6ef1a0646f1baf4dd96d4d4355c6e
Aarkan1 <37023580+Aarkan1@users.noreply.github.com>
2018-09-13 15:30:41 +0200

    Changed recursive method that didn't work

--- Straffuppgift av mr Katra.js
+++ Straffuppgift av mr Katra.js
@@ -3,32 +3,27 @@ public class Main {
     public static void main(String[] args) {
     
         long tal = 600851475143L;
-
-        //long tal = 13195L;
-
+        long recurv = factorial(tal, 2);
+        
         for (int i = 2; i < tal; i++) {
             while (tal % i == 0) {
                 tal = tal / i;
             }
         }
-
         System.out.println(tal);
-
-        System.out.println(factorial(tal, 2));
-
+        System.out.println(recurv);
     }
-
-
+    
     public static long factorial(long tal, int i) {
 
         if (i < tal) {
             if (tal % i == 0) {
                 tal = tal / i;
             }
-            factorial(tal, i = i + 1 );
         }
-
-      return tal;
-
+        if (tal == i){
+            return tal;   
+        }
+        return factorial(tal, i = i + 1 );
     }
 }
